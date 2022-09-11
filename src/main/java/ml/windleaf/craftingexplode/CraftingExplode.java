a863fe0781207e41abb600b9939fed7f4ffd1313
WindLeaf233 <mc.windleaf@foxmail.com>
2022-09-11 21:50:37 +0800

    :tada: begin project

---
+++ src/main/java/ml/windleaf/craftingexplode/CraftingExplode.java
@@ -0,0 +1,11 @@
+package ml.windleaf.craftingexplode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftingExplode extends JavaPlugin {
+  @Override
+  public void onEnable() {}
+
+  @Override
+  public void onDisable() {}
+}
